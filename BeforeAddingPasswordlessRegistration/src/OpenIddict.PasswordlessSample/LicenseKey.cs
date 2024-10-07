1b80a06dd4476dc90bbe87254ddb6e1e10202b75
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2024-10-07 10:01:06 +0100

    Remove unneeded folders.

--- BeforeAddingPasswordlessRegistration/src/OpenIddict.PasswordlessSample/LicenseKey.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OpenIddict.PasswordlessSample;
-
-public static class LicenseKey
-{
-    public const string Licensee = "DEMO";
-    public const string Key = "add-your-license-key-here";
-}
